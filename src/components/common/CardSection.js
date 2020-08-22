67cbe4214667028ae3e09bf66cb4add2a7c32045
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-08-22 16:02:39 +0500

    Changes

--- src/components/common/CardSection.js
+++ src/components/common/CardSection.js
@@ -17,4 +17,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default CardSection;
+export {CardSection};
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -27,4 +27,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Header;
+export {Header};
