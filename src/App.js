f469e453271bb0679ef35d186c23ba7dbf671102
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-08-22 17:27:27 +0500

    Changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
 import firebase from 'firebase';
-import {Header} from './components/common';
-import LoginForm from './components/LoginForm'
+import {Header, Button, Spinner} from './components/common';
+import LoginForm from './components/LoginForm';
 class App extends Component {
+  state = {
+    loggedIn: null,
+  };
   //WARNING! To be deprecated in React v17. Use componentDidMount instead.
-  componentWillMount() {
+  UNSAFE_componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyDrr3Le-7fZ8dNdTm2z5wv_EVwiRv8C4bY',
       authDomain: 'authentication-48b97.firebaseapp.com',
@@ -16,12 +19,31 @@ class App extends Component {
       appId: '1:637616958659:web:90de92caf8f64aa716c676',
       measurementId: 'G-74CHCD1E10',
     });
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({loggedIn: true});
+      } else {
+        this.setState({loggedIn: false});
+      }
+    });
+  }
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>Log out</Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
   }
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
